42aa0fb9819aafa49cfd19d5bf6d474ed33ec9b5
Lorran <lorran.dba@gmail.com>
2017-08-08 12:54:42 -0300

    Add project files.

---
+++ Mini.WebAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace Mini.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ Mini.WebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Mini.WebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
